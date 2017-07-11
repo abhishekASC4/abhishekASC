408c8c44a1ca51be2742f7580f450cfde4ced853
abhishekASC4 <abhishekramjit@ymail.com>
2017-07-11 14:46:21 -0700

    Added Rock Paper Scissors and Condition practice

---
+++ RPS.js
@@ -0,0 +1,46 @@
+var type = ["rock", "paper", "scissors"];
+var userChoice = type[1];
+var comChoice = type[Math.floor(Math.random()*3)];
+
+if(userChoice === type[1]);{
+    if(comChoice == type[1]);{
+        console.log("The computer picked " + comChoice + ". You tied.");
+        exit()
+    };
+    if(comChoice == type[2]);{
+        console.log("The computer picked " + comChoice + ". the computer wins.");
+        exit()
+    };
+    if(comChoice == type[3]);{
+        console.log("The computer picked " + comChoice + ". You win.");
+        exit()
+    };
+};
+if(userChoice === type[2]);{
+    if(comChoice == type[1]);{
+        console.log("The computer picked " + comChoice + ". You win.");
+        exit()
+    };
+    if(comChoice == type[2]);{
+        console.log("The computer picked " + comChoice + ".You tied.");
+        exit()
+    };
+    if(comChoice == type[3]){
+        console.log("The computer picked " + comChoice + ". The computer wins.");
+        exit()
+    };
+};
+if(userChoice === type[3]);{
+    if(comChoice == type[1]);{
+        console.log("The computer picked " + comChoice + ". The computer wins.");
+        exit()
+    };
+    if(comChoice == type[2]);{
+        console.log("The computer picked " + comChoice + ". You win.");
+        exit()
+    }
+    if(comChoice == type[3]);{
+        console.log("The computer picked " + comChoice + ". You tied.");
+        exit()
+    }
+}
