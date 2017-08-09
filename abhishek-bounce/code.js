7d9b93e8275ed4decad36b9b8c4eeba2d47e1e5f
abhishekASC4 <abhishekramjit@ymail.com>
2017-08-09 11:40:45 -0700

    Updated pong website

--- abhishek-bounce/code.js
+++ abhishek-bounce/code.js
@@ -9,6 +9,11 @@ var xPaddle = 300;
 var yPaddle = 590;
 var xPaddle2 = 300;
 var yPaddle2 = 10;
+var canPlay = false;
+var again = false;
+var i = 0
+var ranking = [];
+var newrank = [];
 function updateDB(){
     var newScore = {
         Name: name,
@@ -22,39 +27,46 @@ $('document').ready(function(){
         if(name===''){
             alert("Please enter a user name");
         }else{
-            $('.play').css("display","inline")
+            canPlay = true;
         }
-    })
-    $('.play').click(function(){
-        play = true;
-        draw();
-        loop();
     })
     database.on("child_added", function (rowData) {
         var row = rowData.val();
         var name = row.Name;
         var score = row.Score;
-        var fullText = "<p>" + name + ": " + score + "</p>";
-        $(".leaderboard ").append(fullText);
+        $(".leaderboard").append("<p>"+ name + ": " + score + "</p>" )
+        ranking.push(row)
+        console.log(ranking)
     })
+    
+    // console.log(ranking)
+    // for(var i=0;i<ranking.length;i++){
+    //     for(var b=1;b<ranking.length;b++){
+    //         if(ranking[i-b].Score<ranking[i].Score){
+    //             newrank.push(ranking[i])
+                
+    //         }
+    //     }
+    // }
+    // console.log(newrank)
 })
 var img;
 function setup(){
     createCanvas(600,600)
     background(25,50,5)
     stroke(25,50,5)
-    img = loadImage("playButton.png");
+    // img = loadImage("playButton.png");
 }
 
 function collisionUser(){
-    if ((xPaddle + 100) > x && (xPaddle - 100) < x) {// makes ball bounce off user paddle
+    if ((xPaddle + 75) > x && (xPaddle - 75) < x) {// makes ball bounce off user paddle
         if ((yPaddle + 10) > y && (yPaddle - 3) < y) {
             vy = random(-7, -5)
         }
     }
 }
 function collisionComp() {
-    if ((xPaddle2 + 100) > x && (xPaddle2 - 100) < x) {// makes ball bounce off computer paddle
+    if ((xPaddle2 + 75) > x && (xPaddle2 - 75) < x) {// makes ball bounce off computer paddle
         if ((yPaddle2 + 3) > y && (yPaddle2 - 10) < y) {
             vy = random(5, 7);
         }
@@ -94,13 +106,15 @@ function checkPoints(){
         text("Points: "+ points,200,250)
         noLoop();
         updateDB();
+        again = true;
+        play = false;
     }
 }
 function pong(){
-    fill(25, 50, 5)
-    ellipse(x, y, 20, 20)//clears ball
-    rect(xPaddle, yPaddle, 100, 3)//clears user paddle
-    rect(xPaddle2, yPaddle2, 100, 3)//clears computer paddle
+    background(25, 50, 5)
+    // ellipse(x, y, 20, 20)//clears ball
+    // rect(xPaddle, yPaddle, 100, 3)//clears user paddle
+    // rect(xPaddle2, yPaddle2, 100, 3)//clears computer paddle
     if (0 > x) { //makes ball bounce back on left wall
         vx = random(5, 10);
     } else if (600 < x) { // makes ball bounce back on right wall
@@ -119,16 +133,30 @@ function pong(){
     fill(255)
     ellipse(x, y, 20, 20)//draws ball
     rect(xPaddle, yPaddle, 100, 3)//draws user paddle
-    rect(xPaddle2, yPaddle2, 100, 3)//clears computer paddle
+    rect(xPaddle2, yPaddle2, 100, 3)//draws computer paddle
     checkBall();
+}
+function renderPlay(){
+    fill(0)
+    rect(250, 250, 100, 80, 20)
+    fill(255)
+    triangle(275, 265, 275, 315, 335, 290)
+    if(mouseIsPressed){
+        console.log('pressed')
+        if (mouseX > 150 && mouseX < 350 && mouseY > 170 && mouseY < 330) {
+            play = true;
+            loop();
+        }
+    }
 }
 function draw(){
     if(play === true){
+        fill(25, 50, 5)
+        rect(250, 250, 100, 80, 20)
         pong()
     }else{
-        fill(0)
-        rect(250,250,100,70,20)
-        fill(255)
-        triangle(275,275,275,325,325,250)
+        if(canPlay===true){
+            renderPlay();
+        }
     }
 }
